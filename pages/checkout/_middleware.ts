0533a000e16ed45fdb2aee1dc381c65c5072e2c0
MARCOS JAMPIETRI <marcosjampietri@MARCOSs-MacBook-Pro.local>
2022-04-05 22:28:11 +0100

    changed checkout middleware

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -4,13 +4,13 @@ import jwt, { Secret } from "jsonwebtoken";
 export default async function Middleware(req: NextRequest) {
     const token = req.cookies['auth-token']
     const { pathname } = req.nextUrl
-    // console.log(`log do auth token: ${token} e req.nextUrl ${red}`)
+    console.log(`log do auth token: ${token} e req.nextUrl ${pathname}`)
     // console.log(red)
     // return NextResponse.next()
 
     try {
-        jwt.verify(token, <Secret>process.env["JWT_TOKEN_SECRET"])
-        return NextResponse.next()
+        // jwt.verify(token, <Secret>process.env.JWT_TOKEN_SECRET)
+        if (token) return NextResponse.next()
     } catch {
         return NextResponse.redirect(`/signIU`)
         // return NextResponse.redirect(`/signIU?red=${pathname}`)
